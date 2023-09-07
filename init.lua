c3a710dc656ac132ab13346123ee7f383eafefc7
Matt <matthew.brunetti28@gmail.com>
2023-09-07 18:07:40 -0400

    desktop config

--- init.lua
+++ init.lua
@@ -37,7 +37,7 @@ return {
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
+          "python",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
